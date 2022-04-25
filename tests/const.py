dd0cf460dcb00968bcff27bb472d6e2d8658174a
Andrew Bates <abates@omeganetserv.com>
2022-04-25 07:24:57 -0400

    Initial commit

---
+++ tests/const.py
@@ -0,0 +1,9 @@
+"""Constants for Schedule Sensor tests."""
+from custom_components.schedule_sensor.const import (
+    CONF_PASSWORD,
+)
+from custom_components.schedule_sensor.const import (
+    CONF_USERNAME,
+)
+
+MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
